0263d93f21ac3b8b1e1febcf9a49020b5eea1e5c
qhwa <qhwa@163.com>
2016-12-06 17:03:48 +0800

    add supervisor

--- lib/exhtml.ex
+++ lib/exhtml.ex
@@ -1,10 +1,22 @@
 defmodule Exhtml do
 
+  use Application
+
   @moduledoc """
   Exhtml serves HTML contents.
   It fetches content from backend storage and saves in
   its state. HTML contents are responded to user directly
   from state.
   """
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      worker(Exhtml.Host, [])
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
 
 end
