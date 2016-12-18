9c9742bd7dbb229aa8b314c1ceac79886295d846
qhwa <qhwa@163.com>
2016-12-18 20:59:00 +0800

    bump version to 0.1.0-beta.2

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Exhtml.Mixfile do
   def project do
     [
       app: :exhtml,
-      version: "0.1.0-beta.1",
+      version: "0.1.0-beta.2",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
